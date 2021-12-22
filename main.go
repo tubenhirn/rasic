cbdb9420aca06303e205c0b1fb1939f6926c7691
Johannes Stang <johannes.stang@empiriecom.com>
2021-12-22 22:01:56 +0100

    fix: use default values for main

--- main.go
+++ main.go
@@ -12,18 +12,49 @@ import (
 
 func main() {
 	app := &cli.App{
-		Name:        "cve2issue",
-		Usage:       "create issues from known cve's",
-		Description: "a simple app to create issues (on gitlab.com) from known cve's",
-		Version:     "v0.1.1",
-		Compiled:    time.Now(),
-		Authors: []*cli.Author{
-			{
-				Name:  "Johannes Stang",
-				Email: "tubenhirn@gmail.com",
-			},
+		Name:                 "cve2issue",
+		HelpName:             "",
+		Usage:                "create issues from known cve's",
+		UsageText:            "",
+		ArgsUsage:            "",
+		Version:              "v0.1.1",
+		Description:          "a simple app to create issues (on gitlab.com) from known cve's",
+		Commands:             []*cli.Command{},
+		Flags:                []cli.Flag{},
+		EnableBashCompletion: false,
+		HideHelp:             false,
+		HideHelpCommand:      false,
+		HideVersion:          false,
+		BashComplete: func(*cli.Context) {
 		},
+		Before: func(*cli.Context) error {
+			return nil
+		},
+		After: func(*cli.Context) error {
+			return nil
+		},
+		Action: func(*cli.Context) error {
+			return nil
+		},
+		CommandNotFound: func(*cli.Context, string) {
+		},
+		OnUsageError: func(context *cli.Context, err error, isSubcommand bool) error {
+			return nil
+		},
+		Compiled:  time.Now(),
+		Authors:   []*cli.Author{{Name: "Johannes Stang", Email: "tubenhirn@gmail.com"}},
 		Copyright: "(c) 2021 tubenhirn.com",
+		Reader:    nil,
+		Writer:    nil,
+		ErrWriter: nil,
+		ExitErrHandler: func(context *cli.Context, err error) {
+		},
+		Metadata: map[string]interface{}{},
+		ExtraInfo: func() map[string]string {
+			return nil
+		},
+		CustomAppHelpTemplate:  "",
+		UseShortOptionHandling: false,
 	}
 
 	app.Commands = append(app.Commands, commands.Scan())
