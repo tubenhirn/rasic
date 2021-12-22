abfdde98ebd809f7ca1ca14bdb791bafafa125fb
Johannes Stang <johannes.stang@empiriecom.com>
2021-12-22 18:50:45 +0100

    feat: add first steps for issue command🌛

--- commands/issue.go
+++ commands/issue.go
@@ -6,13 +6,36 @@ import (
 
 func OpenIssue() *cli.Command {
 	return &cli.Command{
-		Name:    "open",
-		Aliases: []string{"o"},
-		Usage:   "open an issue for known cve's",
-		Flags:   []cli.Flag{},
+		Name:        "open",
+		Aliases:     []string{"o"},
+		Usage:       "open an issue for known cve's",
+		UsageText:   "",
+		Description: "",
+		ArgsUsage:   "",
+		Category:    "",
+		BashComplete: func(*cli.Context) {
+		},
+		Before: func(*cli.Context) error {
+			return nil
+		},
+		After: func(*cli.Context) error {
+			return nil
+		},
 		Action: func(c *cli.Context) error {
 			return nil
 		},
+		OnUsageError: func(context *cli.Context, err error, isSubcommand bool) error {
+			return nil
+		},
+		Subcommands:            []*cli.Command{},
+		Flags:                  []cli.Flag{},
+		SkipFlagParsing:        false,
+		HideHelp:               false,
+		HideHelpCommand:        false,
+		Hidden:                 false,
+		UseShortOptionHandling: false,
+		HelpName:               "",
+		CustomHelpTemplate:     "",
 	}
 
 }
